8ff54c5f12ffc8fc47798a1bf0ae5df5514998dd
vaneacal23 <vanessaacostaalmonacid@gmail.com>
2022-07-02 19:02:02 -0500

    creacion de archivos css

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
 });
